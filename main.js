6f993e63c79e0bbd8325500997bc4d15bbb66dc7
Hugo Oliveira <128196219+hugopoliveira14@users.noreply.github.com>
2024-04-04 21:33:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Olá mundo!";
+let myVariable = "Bob";
+myVariable = "Sonic";
+myVariable;
+console.log(myVariable);
+
+const foo = 42
+const resultado = foo + "1";
+console.log(resultado);
+
+let iceCream = "Flocos";
+if (iceCream === "chocolate") {
+    alert("Yay, I love chocolate ice cream!");
+
+} else {
+    alert("Awww, but chocolate is my favorite...");
+
+}
+
+function multiply(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+
+console.log(multiply(4, 7));
